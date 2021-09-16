1fdcad9638a1eab1b0cdf7157ec3808890d4723e
Einar Persson <einar.jpg@gmail.com>
2021-09-16 23:54:39 +0200

    db test

--- packages/lambda/index.js
+++ packages/lambda/index.js
@@ -1,32 +1,38 @@
-import { PutItemCommand } from "@aws-sdk/client-dynamodb";
-import { ddbClient } from "./ddb-client";
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+const REGION = "eu-central-1";
+const ddbClient = new DynamoDBClient({ region: REGION });
+
+const success = (body) => {
+  return buildResponse(200, body);
+};
+
+const failure = (body) => {
+  return buildResponse(500, body);
+};
+
+const buildResponse = (statusCode, body) => ({
+  statusCode: statusCode,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+  },
+  body: JSON.stringify(body),
+});
 
 export const handler = async (event) => {
   const params = {
     TableName: "Analytics",
     Item: {
-      Date: "2012-09-16",
+      Date: { S: "2012-09-17" },
       SessionId: "b09d065d",
-      Event: {
-        v: 5,
-        s: "st",
-      },
     },
   };
 
-  const run = async () => {
-    try {
-      const data = await ddbClient.send(new PutItemCommand(params));
-      console.log(data);
-      return data;
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  run();
-
-  const response = {
-    statusCode: 204,
-  };
-  return response;
+  try {
+    await ddbClient.send(new PutItemCommand(params));
+    return success(params.Item);
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
 };
